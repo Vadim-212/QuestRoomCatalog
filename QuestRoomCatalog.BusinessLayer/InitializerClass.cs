34f3c43e45e292125ae8db925943ab2e87a40a7d
Вадим <vadim.212@inbox.ru>
2019-10-26 00:24:25 +0600

    Add initializer class for db

---
+++ QuestRoomCatalog.BusinessLayer/InitializerClass.cs
@@ -0,0 +1,17 @@
+﻿using QuestRoomCatalog.DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestRoomCatalog.BusinessLayer
+{
+    public static class InitializerClass
+    {
+        public static Model1Initializer Initialize()
+        {
+            return new Model1Initializer();
+        }
+    }
+}
---
+++ QuestRoomCatalog.DataLayer/Entities/Model1Initializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestRoomCatalog.DataLayer.Entities
+{
+    public class Model1Initializer: DropCreateDatabaseAlways<Model1>
+    {
+        protected override void Seed(Model1 db)
+        {
+            db.Roles.Add(new Roles() { RoleName = "Administrator" });
+            db.Roles.Add(new Roles() { RoleName = "User" });
+            db.Users.Add(new Users() { UserName = "Admin", RoleId = 1 });
+            db.Users.Add(new Users() { UserName = "User", RoleId = 2 });
+        }
+    }
+}
